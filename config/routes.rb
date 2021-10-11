834c9a00654dd7ad7a584428bec57e1cf422d45a
Lispooh13 <riaya1203@gmail.com>
2021-10-11 09:13:37 +0000

    [Add]マイページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
@@ -8,6 +11,6 @@ Rails.application.routes.draw do
   
   resources :posts
   
-  
+  resources :users, only: [:show, :edit, :update]
   
 end
