c1a8dd92c4999985c24906e6ffed48c72b360766
Lispooh13 <riaya1203@gmail.com>
2021-10-23 06:26:25 +0000

    [Start]タグ検索

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,8 +1,9 @@
 class SearchesController < ApplicationController
   before_action :authenticate_user!,except:[:search]
+  
   def search
    @posts = Post.search(params[:keyword])
-   @posts_page = Post.page(params[:page]).limit(10) #全部取ってきちゃう…Postモデルに記載してるため
+   @posts_page = Post.page(params[:page]).limit(9) #全部取ってきちゃう…Postモデルに記載してるため
 
   end
 end
