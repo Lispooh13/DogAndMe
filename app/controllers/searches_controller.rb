861eef21ae596f5d3049413151aa5b764a79ac5b
Lispooh13 <riaya1203@gmail.com>
2021-10-23 13:05:54 +0000

    [Update]タグ検索

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,8 +1,11 @@
 class SearchesController < ApplicationController
   before_action :authenticate_user!,except:[:search]
+
   def search
-   @posts = Post.search(params[:keyword])
-   @posts_page = Post.page(params[:page]).limit(10) #全部取ってきちゃうPostモデルに記載してるため
+   posts = Post.search(params[:keyword])
+   @posts = Kaminari.paginate_array(posts).page(params[:page]).per(3)
+
+   # @search = Post.page(params[:page]).order(created_at: :desc).per(12) #全部取ってきちゃうPostモデルに記載してるため
 
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -23,19 +23,29 @@ class Post < ApplicationRecord
 
 
 #場所名と住所、説明からの部分検索
-  def self.search(search)
-    if search != ""
-      @hashtag = Hashtag.where('hashname LIKE(?)', "%#{search}%")
-      @post = Post.where(['place LIKE(?) OR address LIKE(?) OR body LIKE(?)', "%#{search}%", "%#{search}%","%#{search}%"])
-      if @hashtag.present?
-        tagposts = HashtagPost.where(hashtag_id: @hashtag.ids)
-        tagpost = tagposts.pluck(:post_id)
-        post = Post.where(id: tagpost)
-        @post += post
-      end
-    else
-      Post.all
-    end
+  def self.search(keyword)
+    # if search != ""
+    #   @hashtag = Hashtag.where('hashname LIKE(?)', "%#{search}%")
+    #   @post = Post.where(['place LIKE(?) OR address LIKE(?) OR body LIKE(?)', "%#{search}%", "%#{search}%","%#{search}%"])
+    #   if @hashtag.present?
+    #     tagposts = HashtagPost.where(hashtag_id: @hashtag.ids)
+    #     tagpost = tagposts.pluck(:post_id)
+    #     post = Post.where(id: tagpost)
+    #    @post += post
+    #  end
+    # else
+    #  Post.all
+    # end
+
+    return [] if keyword.blank?
+
+    hashtag_ids = Hashtag.where('hashname LIKE ?', "%#{keyword}%").pluck(:id)
+    hashtag_post_ids = HashtagPost.where(hashtag_id: hashtag_ids).pluck(:post_id)
+    hashtag_posts = Post.where(id: hashtag_post_ids)
+    posts = Post.where(['place LIKE ? OR address LIKE ? OR body LIKE ?', "%#{keyword}%", "%#{keyword}%", "%#{keyword}%"])
+    result = (posts + hashtag_posts).uniq
+    result.sort_by! { |data| data[:created_at] }.reverse!
+
   end
 
 # 通知
