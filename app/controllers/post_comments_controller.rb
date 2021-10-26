b89538196343eb8939774f656c8c05901210f8aa
Lispooh13 <riaya1203@gmail.com>
2021-10-26 14:17:17 +0000

    [Fix]

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -6,7 +6,6 @@ class PostCommentsController < ApplicationController
     @post_comment = PostComment.new(post_comment_params)
     @post_comment.post_id = @post.id
     @post_comment.user_id = current_user.id
-    @post = @post_comment.post
     @post_comment.save
     @post.create_notification_post_comment!(current_user, @post_comment.id)
 
