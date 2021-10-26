adbccd22d59fdeb39beccb8a5a703e83cea35b90
Lispooh13 <riaya1203@gmail.com>
2021-10-26 08:52:31 +0000

    [Fix]

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  #top関連
   root to: 'homes#top'
   get '/about' => 'homes#about'
   
